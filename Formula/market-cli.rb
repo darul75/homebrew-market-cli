bde804a42627c77acd891fdef48690b07179b73c
Julien Valéry <julien.v@apsulis.io>
2026-03-25 20:56:25 +0100

    bump v0.2.2

--- Formula/market-cli.rb
+++ Formula/market-cli.rb
@@ -1,13 +1,13 @@
 class MarketCli < Formula
   desc "Stock portfolio tracking CLI with real-time updates"
   homepage "https://github.com/darul75/market-cli"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/darul75/market-cli/releases/download/v#{version}/market-cli-macos-arm64.tar.gz"
-      sha256 "9e2aa43b7bba2cc8429a06863aec1320146f9d3bae62e069c0b399aa97f127e5"
+      sha256 "037effb6a149696e3a502b612e7a2a1d95a82ddbf6a8f6b022ef311f91dc3272"
     end
   end
 
